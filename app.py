f81e272e99cf8a1b7a510af436ada650938f310b
slmanalodani <slman.alodani@student.nhlstenden.com>
2026-03-02 09:10:55 +0100

    adding the routes

--- app.py
+++ app.py
@@ -1,3 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = (__name__)
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/register")
+def signup():
+    
+    return render_template("register.html")
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
